a9bd033f4277a9fe44f47649ec879decb3096a25
Mohammad Samanipour <mohamadsamanipoor@yahoo.com>
2022-02-13 00:42:57 +0330

    Add timer

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 type problem struct {
@@ -15,6 +16,7 @@ type problem struct {
 
 func main() {
 	csvFileName := flag.String("csv", "problems.csv", "a csv file in the default format")
+	timeLimit := flag.Int("limit", 30, "the time limit for quiz")
 	flag.Parse()
 
 	file, err := os.Open(*csvFileName)
@@ -31,16 +33,26 @@ func main() {
 	}
 	problems := parseLines(lines)
 
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
 	currect := 0
+
+problemLoop:
 	for i, p := range problems {
-		fmt.Printf("Problem #%d : %s = \n", i+1, p.q)
-		var answer string
-		fmt.Scanf("%s\n", &answer)
-		if answer == p.a {
-			fmt.Println("Correct answer!")
-			currect++
-		} else {
-			fmt.Println("Wrong answer!")
+		fmt.Printf("Problem #%d : %s = ", i+1, p.q)
+		answerCh := make(chan string)
+		go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerCh <- answer
+		}()
+		select {
+		case <-timer.C:
+			fmt.Println()
+			break problemLoop
+		case answer := <-answerCh:
+			if answer == p.a {
+				currect++
+			}
 		}
 	}
 	fmt.Printf("You scored %d out of %d.\n", currect, len(problems))
